e5cfd9333de58417445734e0d3821423f4bd74a7
SVA-BL00 <sarav.notes@gmail.com>
2024-11-22 14:37:41 -0600

    Changes

--- GoodSoup/app/(tabs)/_layout.tsx
+++ GoodSoup/app/(tabs)/_layout.tsx
@@ -1,29 +1,33 @@
 import { Tabs } from 'expo-router';
-import React from 'react';
-import  { MainMenu } from '@/components/MainMenuC';
-import { ImageBackground, StyleSheet } from 'react-native';
-
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
+import React, {  useRef, useState } from 'react';
+import { MainMenu } from '@/components/MainMenuC';
+import { UnityGame, UnityGameRef } from '@/components/UnityGame';
+import { View } from 'react-native';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+  const [showMainMenu, setShowMainMenu] = useState<boolean>(false); // Start with menu hidden
+  const unityGameRef = useRef<UnityGameRef>(null);
+
+  const handleUnityMessage = (message: string) => {
+    if(message === 'Start'){
+      setShowMainMenu(true);
+    }else if(message === 'Game loaded'){
+      setShowMainMenu(false);
+    }
+  };
+
+  const handleModalClose = () => {
+    if (unityGameRef.current) {
+      unityGameRef.current.resumeUnity();
+    }
+  };
 
   return (
-    <ImageBackground 
-        source={require('../../assets/images/Menu.png')} 
-        style={styles.backgroundImage}
-    >
-        <MainMenu />
-    </ImageBackground>
-);
+    <View style={{ flex: 1 }}>
+      {showMainMenu && <MainMenu onCloseModal={handleModalClose} />}
+      <UnityGame ref={unityGameRef} onUnityMessage={handleUnityMessage} />
+    </View>
+  );
 }
-
-const styles = StyleSheet.create({
-  backgroundImage: {
-      flex: 1, // Takes full available space
-      justifyContent: 'center', // Center child components
-      alignItems: 'center', // Center child components
-  },
-});
